90e5e806f84e34eb17fdf5d341f4453850a1649d
Claudio Savino <claudio.savino@me.com>
2016-08-10 10:03:41 +0200

    refactor: added mobx strict mode

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
+import { useStrict } from 'mobx';
 import Form from './form';
+
+/**
+  Enables / disables strict mode globally.
+  In strict mode, it is not allowed to
+  change any state outside of an action
+ */
+useStrict(true);
 
 export default Form;
