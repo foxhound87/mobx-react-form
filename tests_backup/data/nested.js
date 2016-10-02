d65538c41694c90b26e6be6e2a83854a5e39e499
Claudio Savino <claudio.savino@me.com>
2016-10-02 19:33:54 +0200

    nested-fields

--- tests_backup/data/nested.js
+++
@@ -1,9 +0,0 @@
-// test data
-import $A from './form.nested.a';
-import $B from './form.nested.b';
-import $C from './form.nested.c';
-import $D from './form.nested.d';
-import $E from './form.nested.e';
-import $F from './form.nested.f';
-
-export default { $A, $B, $C, $D, $E, $F };
--- tests_backup/promises/index.js
+++
@@ -1,3 +0,0 @@
-import validate from './validate';
-
-export default { validate };
