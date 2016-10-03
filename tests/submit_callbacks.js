729a8975913d661922afebb3e82f41c82fe5b38f
Volodymyr Shevhcuk <volodymyr.sh21.gmail.com>
2016-10-04 01:37:49 +0300

    Added tests for form submit

---
+++ tests/submit_callbacks.js
@@ -0,0 +1,24 @@
+import { expect } from 'chai';
+import $ from './data'; // FORMS
+
+describe('Form submit() decoupled callback', () => {
+  // $I
+  it('$I.submit() should call onSuccess callback on valid form', (done) => {
+    $.$I.submit({
+      onSuccess: (form) => {
+        expect(form.isValid).to.be.true;  // eslint-disable-line
+        done();
+      },
+    });
+  });
+
+  // $N
+  it('$N.submit() should call onError callback on invalid form', (done) => {
+    $.$N.submit({
+      onError: (form) => {
+        expect(form.isValid).to.be.false;  // eslint-disable-line
+        done();
+      },
+    });
+  });
+});
