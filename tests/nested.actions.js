15bcedcd8f92a4b78d7ce626ea2d49fd01347cba
Claudio Savino <claudio.savino@me.com>
2016-12-29 14:46:33 +0100

    test: allowed props

--- tests/nested.actions.js
+++ tests/nested.actions.js
@@ -6,6 +6,9 @@ describe('Form error test', () => {
   it('$A select() should throw error', () =>
     expect(() => $.$A.select('some.test')).to.throw(Error));
 
+  it('$A get() should throw error', () =>
+    expect(() => $.$A.get('not-allowed-prop')).to.throw(Error));
+
   it('$T del() should throw error', () =>
     expect(() => $.$T.$('notIncrementalFields').del(99)).to.throw(Error));
 });
