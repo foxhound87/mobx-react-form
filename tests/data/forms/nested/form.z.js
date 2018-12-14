783e9af621a51ebc981904a00d3a5e251fe86464
yuan <yuan@rakenapp.com>
2018-12-14 16:16:42 +1300

    add testcase

---
+++ tests/data/forms/nested/form.z.js
@@ -0,0 +1,32 @@
+import validatorjs from 'validatorjs';
+import { Form } from '../../../../src';
+
+const fields = [
+  'data[].f1',
+  'data[].f2',
+  'data[].f3',
+  'data[].f4',
+  'data[].f5',
+  'data[].f6',
+  'data[].f7',
+];
+
+const values = {
+  data: Array.from(Array(300).keys()).map(idx => {
+    return {
+      f1: Math.random(),
+      f2: Math.random(),
+      f3: Math.random(),
+      f4: Math.random(),
+      f5: Math.random(),
+      f6: Math.random(),
+      f7: Math.random()
+    };
+  })
+}
+
+const plugins = { dvr: validatorjs };
+
+export default new Form({
+  fields, values,
+}, { plugins, name: 'Nested-R' });
