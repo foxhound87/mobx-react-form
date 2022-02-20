4bb66a937131268fb4db48d048733fec30e7fc95
foxhound87 <claudio.savino@me.com>
2022-02-20 22:03:12 +0100

    chore: Validator.ts

---
+++ src/models/ValidatorInterface.ts
@@ -0,0 +1,19 @@
+export default interface ValidatorInterface {
+  promises: Promise<any>[];
+  form: any;
+  drivers: any;
+  plugins: ValidationPlugins;
+  error: string | null;
+  initDrivers(): void;
+  validate(opt: any, obj: any): Promise<any>;
+  validateField(opt: any): void;
+  validateRelatedFields(field: any, showErrors: boolean): void;
+  checkSVKValidationPlugin(): void;
+}
+
+export interface ValidationPlugins {
+  vjf: any;
+  dvr: any;
+  svk: any;
+  yup: any;
+}
