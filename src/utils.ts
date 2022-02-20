65eed137c904ef6d310195b3afb09726f084dbad
foxhound87 <claudio.savino@me.com>
2022-02-20 15:04:17 +0100

    chore: utils.ts

--- src/utils.ts
+++ src/utils.ts
@@ -3,31 +3,30 @@ import { values as mobxValues, keys as mobxKeys } from "mobx";
 
 import { props } from "./props";
 
-const getObservableMapValues = (observableMap) =>
-  mobxValues ? mobxValues(observableMap) : observableMap.values();
+const getObservableMapValues = (observableMap: any) =>
+  mobxValues(observableMap);
 
-const getObservableMapKeys = (observableMap) =>
-  mobxValues ? mobxKeys(observableMap) : observableMap.keys();
+const getObservableMapKeys = (observableMap: any) => mobxKeys(observableMap);
 
 const checkObserveItem =
-  (change) =>
-  ({ key, to, type, exec }) =>
+  (change: any) =>
+  ({ key, to, type, exec }: any) =>
     change.type === type &&
     change.name === key &&
     change.newValue === to &&
     exec.apply(change, [change]);
 
-const checkObserve = (collection) => (change) =>
+const checkObserve = (collection: object[]) => (change: any) =>
   collection.map(checkObserveItem(change));
 
-const checkPropType = ({ type, data }) => {
-  let $check;
+const checkPropType = ({ type, data }: any) => {
+  let $check: any;
   switch (type) {
     case "some":
-      $check = ($data) => _.some($data, Boolean);
+      $check = ($data: object) => _.some($data, Boolean);
       break;
     case "every":
-      $check = ($data) => _.every($data, Boolean);
+      $check = ($data: object) => _.every($data, Boolean);
       break;
     default:
       $check = null;
@@ -35,7 +34,7 @@ const checkPropType = ({ type, data }) => {
   return $check(data);
 };
 
-const hasProps = ($type, $data) => {
+const hasProps = ($type: any, $data: any) => {
   let $props;
   switch ($type) {
     case "booleans":
@@ -69,7 +68,7 @@ const hasProps = ($type, $data) => {
 /**
   Check Allowed Properties
 */
-const allowedProps = (type, data) => {
+const allowedProps = (type: string, data: string[]) => {
   if (hasProps(type, data)) return;
   const $msg = "The selected property is not allowed";
   throw new Error(`${$msg} (${JSON.stringify(data)})`);
@@ -78,13 +77,13 @@ const allowedProps = (type, data) => {
 /**
   Throw Error if undefined Fields
 */
-const throwError = (path, fields, msg = null) => {
+const throwError = (path: string, fields: any, msg: null | string = null) => {
   if (!_.isNil(fields)) return;
   const $msg = _.isNil(msg) ? "The selected field is not defined" : msg;
   throw new Error(`${$msg} (${path})`);
 };
 
-const pathToStruct = (path) => {
+const pathToStruct = (path: string): string => {
   let struct;
   struct = _.replace(path, new RegExp("[.]\\d+($|.)", "g"), "[].");
   struct = _.replace(struct, "..", ".");
@@ -92,25 +91,26 @@ const pathToStruct = (path) => {
   return struct;
 };
 
-const hasSome = (obj, keys) => _.some(keys, _.partial(_.has, obj));
+const hasSome = (obj: any, keys: any) => _.some(keys, _.partial(_.has, obj));
 
-const isStruct = (struct) => _.isArray(struct) && _.every(struct, _.isString);
+const isStruct = (struct: any) =>
+  _.isArray(struct) && _.every(struct, _.isString);
 
-const isEmptyArray = (field) => _.isEmpty(field) && _.isArray(field);
+const isEmptyArray = (field: any) => _.isEmpty(field) && _.isArray(field);
 
-const isArrayOfObjects = (fields) =>
+const isArrayOfObjects = (fields: any) =>
   _.isArray(fields) && _.every(fields, _.isPlainObject);
 
-const $getKeys = (fields) =>
+const $getKeys = (fields: any) =>
   _.union(_.map(_.values(fields), (values) => _.keys(values))[0]);
 
-const hasUnifiedProps = ({ fields }) =>
+const hasUnifiedProps = ({ fields }: any) =>
   !isStruct({ fields }) && hasProps("field", $getKeys(fields));
 
-const hasSeparatedProps = (initial) =>
+const hasSeparatedProps = (initial: any) =>
   hasSome(initial, props.separated) || hasSome(initial, props.validation);
 
-const allowNested = (field, strictProps) =>
+const allowNested = (field: any, strictProps: boolean): boolean =>
   _.isObject(field) &&
   !_.isDate(field) &&
   !_.has(field, "fields") &&
@@ -122,36 +122,39 @@ const allowNested = (field, strictProps) =>
   ]) ||
     strictProps);
 
-const parseIntKeys = (fields) =>
+const parseIntKeys = (fields: any) =>
   _.map(getObservableMapKeys(fields), _.ary(_.toNumber, 1));
 
-const hasIntKeys = (fields) => _.every(parseIntKeys(fields), _.isInteger);
+const hasIntKeys = (fields: any): boolean =>
+  _.every(parseIntKeys(fields), _.isInteger);
 
-const maxKey = (fields) => {
+const maxKey = (fields: any): number => {
   const max = _.max(parseIntKeys(fields));
   return _.isUndefined(max) ? 0 : max + 1;
 };
 
-const uniqueId = (field) =>
+const uniqueId = (field: any): string =>
   _.uniqueId(
     [_.replace(field.path, new RegExp("\\.", "g"), "-"), "--"].join("")
   );
 
-const $isEvent = (obj) => {
+const $isEvent = (obj: any): boolean => {
   if (_.isNil(obj) || typeof Event === "undefined") return false;
   return obj instanceof Event || !_.isNil(obj.target); // eslint-disable-line
 };
 
-const $hasFiles = ($) => $.target.files && $.target.files.length !== 0;
+const $hasFiles = ($: any): boolean =>
+  $.target.files && $.target.files.length !== 0;
 
-const $isBool = ($, val) => _.isBoolean(val) && _.isBoolean($.target.checked);
+const $isBool = ($: any, val: any): boolean =>
+  _.isBoolean(val) && _.isBoolean($.target.checked);
 
-const $try = (...args) => {
-  let found = null;
+const $try = (...args: any) => {
+  let found: boolean | null = null;
 
   args.map(
     (
-      val // eslint-disable-line
+      val: any // eslint-disable-line
     ) => found === null && !_.isNil(val) && (found = val)
   );
 
