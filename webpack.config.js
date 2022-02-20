3ed9c0a4b6cb212bdd0dd06b006dcb2bad4de88c
foxhound87 <claudio.savino@me.com>
2022-02-20 13:11:20 +0100

    chore: updated eslint

---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+const LodashModuleReplacementPlugin = require("lodash-webpack-plugin");
+const { join } = require("path");
+
+const MINIFY = process.env.MINIFY === "YES";
+
+const rules = [
+  {
+    test: /\.js$/,
+    loader: "ts-loader",
+    include: join(__dirname, "src"),
+  },
+  {
+    test: /\.json$/,
+    loader: "json-loader",
+  },
+];
+
+module.exports = {
+  mode: MINIFY ? "production" : "development",
+  devtool: "source-map",
+  entry: {
+    MobxReactForm: "./src/index",
+    MobxReactFormValidatorVJF: "./src/validators/VJF",
+    MobxReactFormValidatorDVR: "./src/validators/DVR",
+    MobxReactFormValidatorSVK: "./src/validators/SVK",
+    MobxReactFormValidatorYUP: "./src/validators/YUP",
+  },
+  output: {
+    path: join(__dirname, "umd"),
+    filename: MINIFY ? "[name].umd.min.js" : "[name].umd.js",
+    library: "[name]",
+    libraryTarget: "umd",
+  },
+  resolve: {
+    modules: ["node_modules"],
+    extensions: [".js", ".json"],
+  },
+  externals: {
+    mobx: "mobx",
+    lodash: "_",
+  },
+  module: { rules },
+  plugins: [new LodashModuleReplacementPlugin()],
+};
