17a0594ad01f0317c7e27586d7bac83c35ba49b2
foxhound87 <claudio.savino@me.com>
2022-02-20 17:40:23 +0100

    chore: update state interface

---
+++ src/State.ts
@@ -0,0 +1,176 @@
+import StateInterface, { RuntimeMode } from "./models/StateInterface";
+import { observe } from "mobx";
+import _ from "lodash";
+
+import Options from "./Options";
+import Bindings from "./Bindings";
+
+import {
+  props,
+  hasUnifiedProps,
+  hasSeparatedProps,
+  checkObserve,
+  isStruct,
+  $try,
+} from "./utils";
+
+import OptionsInterface from "./models/OptionsInterface";
+import BindingsInterface from "./models/BindingsInterface";
+
+export default class State implements StateInterface {
+  mode = RuntimeMode.mixed;
+
+  strict = false;
+
+  form: any;
+
+  options: OptionsInterface;
+
+  bindings: BindingsInterface;
+
+  $struct: string[] = [];
+
+  $extra: any;
+
+  disposers = {
+    interceptor: {},
+    observer: {},
+  };
+
+  initial = {
+    props: {},
+    fields: {},
+  };
+
+  current = {
+    props: {},
+    fields: {},
+  };
+
+  constructor({ form, initial, options, bindings }: any) {
+    this.set("form", form);
+    this.initProps(initial);
+    this.options = new Options();
+    this.options.set(options);
+    this.bindings = new Bindings();
+    this.bindings.register(bindings);
+    this.observeOptions();
+  }
+
+  initProps(initial: any): void {
+    const initialProps: any = _.pick(initial, [
+      ...props.separated,
+      ...props.validation,
+      ...props.function,
+      ...props.handlers,
+    ]);
+
+    this.set("initial", "props", initialProps);
+
+    const $unified = hasUnifiedProps(initial);
+    const $separated = hasSeparatedProps(initial);
+
+    if ($unified && $separated) {
+      console.warn(
+        // eslint-disable-line
+        "WARNING: Your mobx-react-form instance ",
+        this.form.name,
+        " is running in MIXED Mode (Unified + Separated) as fields properties definition.",
+        "This mode is experimental, use it at your own risk, or use only one mode."
+      );
+    }
+
+    if (($separated || isStruct(initial.fields)) && !$unified) {
+      const struct: any = $try(initial.struct || initial.fields);
+      this.struct(struct);
+      this.strict = true;
+      this.mode = RuntimeMode.separated;
+      return;
+    }
+
+    this.struct(initial.struct);
+    this.mode = RuntimeMode.unified;
+  }
+
+  /**
+    Get/Set Fields Structure
+  */
+  struct(data: string[] = []): string[] {
+    if (data) this.$struct = data;
+    return this.$struct;
+  }
+
+  /**
+    Get Props/Fields
+  */
+  get(type: any, subtype: any): any {
+    return _.get(this, subtype ? [type, subtype].join(".") : type);
+  }
+
+  /**
+    Set Props/Fields
+  */
+  set(type: string, subtype: string, state: any = null): void {
+    if (type === "form") {
+      // subtype is the form here
+      this.form = subtype;
+    }
+
+    if (type === "initial") {
+      Object.assign(_.get(this.initial, subtype), state);
+      Object.assign(_.get(this.current, subtype), state);
+    }
+
+    if (type === "current") {
+      Object.assign(_.get(this.current, subtype), state);
+    }
+  }
+
+  extra(data = null): any | null {
+    if (_.isString(data)) return _.get(this.$extra, data);
+    if (data === null) return this.$extra;
+    this.$extra = data;
+    return null;
+  }
+
+  observeOptions(): void {
+    // Fix Issue #201
+    observe(
+      this.options.options,
+      checkObserve([
+        {
+          // start observing fields validateOnChange
+          type: "update",
+          key: "validateOnChange",
+          to: true,
+          exec: () =>
+            this.form.each((field: any) => field.observeValidationOnChange()),
+        },
+        {
+          // stop observing fields validateOnChange
+          type: "update",
+          key: "validateOnChange",
+          to: false,
+          exec: () =>
+            this.form.each((field: any) => field.disposeValidationOnChange()),
+        },
+        {
+          // start observing fields validateOnBlur
+          type: "update",
+          key: "validateOnBlur",
+          to: true,
+          exec: () =>
+            this.form.each((field: any) => field.observeValidationOnBlur()),
+        },
+        {
+          // stop observing fields validateOnBlur
+          type: "update",
+          key: "validateOnBlur",
+          to: false,
+          exec: () =>
+            this.form.each((field: any) => field.disposeValidationOnBlur()),
+        },
+      ])
+    );
+  }
+}
---
+++ src/models/StateInterface.ts
@@ -0,0 +1,41 @@
+import BindingsInterface from "./BindingsInterface";
+import OptionsInterface from "./OptionsInterface";
+
+export enum RuntimeMode {
+  mixed = "mixed",
+  unified = "unified",
+  separated = "separated",
+}
+
+export default interface StateInterface {
+  mode: RuntimeMode;
+  strict: boolean;
+  form: any;
+  options: OptionsInterface;
+  bindings: BindingsInterface;
+
+  $struct: string[];
+  $extra: any;
+
+  disposers: {
+    interceptor: any;
+    observer: any;
+  };
+
+  initial: {
+    props: any;
+    fields: any;
+  };
+
+  current: {
+    props: any;
+    fields: any;
+  };
+
+  initProps(initial: any): void;
+  struct(data: string[]): string[];
+  get(type: any, subtype: any): any;
+  set(type: string, subtype: string, state: any): void;
+  extra(data: any): any | null;
+  observeOptions(): void;
+}
