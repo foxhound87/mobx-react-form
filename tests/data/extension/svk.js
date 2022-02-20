bf2ef480e601969ef8cc724d86b509d0d133482c
foxhound87 <claudio.savino@me.com>
2022-02-20 13:29:10 +0100

    chore: updated configs

--- tests/data/extension/svk.js
+++ tests/data/extension/svk.js
@@ -1,29 +1,32 @@
-import simulateAsyncFindUserCall from './_.async';
+import simulateAsyncFindUserCall from "./_.async";
 
 const extend = {
   keywords: {
     range: {
-      type: 'number',
+      type: "number",
       compile: (sch, parentSchema) => {
         const min = sch[0];
         const max = sch[1];
 
-        return (parentSchema.exclusiveRange === true)
-          ? data => (data > min) && (data < max)
-          : data => (data >= min) && (data <= max);
+        return parentSchema.exclusiveRange === true
+          ? (data) => data > min && data < max
+          : (data) => data >= min && data <= max;
       },
     },
     checkUser: {
       async: true,
       validate: (field, value) =>
-        simulateAsyncFindUserCall({ [field]: value })
-          .then(items => (items.length === 0)),
+        simulateAsyncFindUserCall({ [field]: value }).then(
+          (items) => items.length === 0
+        ),
     },
   },
   // formats: {},
 };
 
 export default ({ validator }) => {
-  Object.keys(extend.keywords).forEach(key => validator.addKeyword(key, extend.keywords[key]));
+  Object.keys(extend.keywords).forEach((key) =>
+    validator.addKeyword(key, extend.keywords[key])
+  );
   // Object.keys(extend.formats).forEach(key => validator.addFormat(key, extend.formats[key]));
 };
