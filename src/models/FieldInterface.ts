dd5cd7097b36878fce888bf9d9f74ed33a929e62
foxhound87 <claudio.savino@me.com>
2022-02-21 11:26:50 +0100

    chore: field interface

---
+++ src/models/FieldInterface.ts
@@ -0,0 +1,73 @@
+import OptionsModel from "./OptionsModel";
+
+export default interface FieldInterface {
+  id: string | undefined;
+  key: string | undefined;
+  name: string | undefined;
+  path: string | undefined;
+  options: OptionsModel | undefined;
+  type: string | undefined;
+  label: string | undefined;
+  placeholder: string | undefined;
+  extra: any;
+  errorSync: string | null;
+  errorAsync: string | null;
+  validationErrorStack: string[];
+  validationFunctionsData: any[];
+  debouncedValidation: any;
+  autoFocus: boolean;
+  showError: boolean;
+  checkValidationErrors: boolean;
+  checked: any;
+  value: any;
+  initial: any;
+  default: any;
+  actionRunning: boolean;
+  bindings: any;
+  disabled: boolean;
+  related: string[] | undefined;
+  rules: string[] | undefined;
+  validators: any[] | undefined;
+  validatedValue: any;
+  error: string | null;
+  hasError: boolean;
+  isValid: boolean;
+  isDefault: boolean;
+  isDirty: boolean;
+  isPristine: boolean;
+  isEmpty: boolean;
+  resetting: boolean;
+  clearing: boolean;
+  focused: boolean;
+  blurred: boolean;
+  touched: boolean;
+  changed: boolean;
+  deleted: boolean;
+  // handlers
+  onChange(args: any): any;
+  onToggle(args: any): any;
+  onBlur(args: any): any;
+  onFocus(args: any): any;
+  onDrop(args: any): any;
+  // methods: {
+  setupField(
+    key: string,
+    path: string,
+    data: any,
+    props: any,
+    update: boolean
+  ): void;
+  getComputedProp(key: any): any;
+  checkValidationPlugins(): void;
+  initNestedFields(field: any, update: boolean): void;
+  invalidate(message: string, async: boolean): void;
+  setValidationAsyncData(valid: boolean, message: string): void;
+  resetValidation(deep: boolean): void;
+  clear(deep: boolean): void;
+  reset(deep: boolean): void;
+  focus(): void;
+  showErrors(show: boolean): void;
+  showAsyncErrors(): void;
+  observeValidationOnBlur(): void;
+  observeValidationOnChange(): void;
+}
