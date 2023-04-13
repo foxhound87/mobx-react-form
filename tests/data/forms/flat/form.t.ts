foxhound87/mobx-react-form

commit 26123aa7718871c6e9ddec67e1699e949d096ece

refactor: updated models

src/Base.ts
@@ -36,6 +36,8 @@ import {
   parseCheckOutput,
   pathToFieldsTree,
 } from "./parser";
+import { FieldPropsEnum } from "./models/FieldProps";
+import { OptionsEnum } from "./models/OptionsModel";
 export default class Base implements BaseInterface {
   noop = () => {};
 
@@ -132,8 +134,8 @@ export default class Base implements BaseInterface {
   }
 
   get changed(): number {
-    return !_.isNil(this.path) && this.hasNestedFields 
-      ? this.reduce((acc: number, field: FieldInterface) => (acc + field.changed), 0) 
+    return !_.isNil(this.path) && this.hasNestedFields
+      ? this.reduce((acc: number, field: FieldInterface) => (acc + field.changed), 0)
       : this.$changed;
   }
 
@@ -161,7 +163,7 @@ export default class Base implements BaseInterface {
     Event Handler: On Clear
   */
   onClear = (...args: any): any =>
-    this.execHandler("onClear", args, (e: Event) => {
+    this.execHandler(FieldPropsEnum.onClear, args, (e: Event) => {
       e.preventDefault();
       (this as any).clear(true);
     });
@@ -170,7 +172,7 @@ export default class Base implements BaseInterface {
     Event Handler: On Reset
   */
   onReset = (...args: any): any =>
-    this.execHandler("onReset", args, (e: Event) => {
+    this.execHandler(FieldPropsEnum.onReset, args, (e: Event) => {
       e.preventDefault();
       (this as any).reset(true);
     });
@@ -179,7 +181,7 @@ export default class Base implements BaseInterface {
     Event Handler: On Submit
    */
   onSubmit = (...args: any): any =>
-    this.execHandler("onSubmit", args, (e: Event, o = {}) => {
+    this.execHandler(FieldPropsEnum.onSubmit, args, (e: Event, o = {}) => {
       e.preventDefault();
       this.submit(o);
     });
@@ -188,7 +190,7 @@ export default class Base implements BaseInterface {
     Event Handler: On Add
   */
   onAdd = (...args: any): any =>
-    this.execHandler("onAdd", args, (e: Event, val: any) => {
+    this.execHandler(FieldPropsEnum.onAdd, args, (e: Event, val: any) => {
       e.preventDefault();
       this.add($isEvent(val) ? null : val);
     });
@@ -197,7 +199,7 @@ export default class Base implements BaseInterface {
     Event Handler: On Del
   */
   onDel = (...args: any): any =>
-    this.execHandler("onDel", args, (e: Event, path: string) => {
+    this.execHandler(FieldPropsEnum.onDel, args, (e: Event, path: string) => {
       e.preventDefault();
       this.del($isEvent(path) ? this.path : path);
     });
@@ -206,7 +208,7 @@ export default class Base implements BaseInterface {
     Initializer
   */
   initFields(initial: any, update: boolean = false): void {
-    const fallback = this.state.options.get("fallback");
+    const fallback = this.state.options.get(OptionsEnum.fallback);
     const $path = (key: string) => _.trimStart([this.path, key].join("."), ".");
 
     let fields;
@@ -312,13 +314,13 @@ export default class Base implements BaseInterface {
 
     return (
       this.validate({
-        showErrors: this.state.options.get("showErrorsOnSubmit", this),
+        showErrors: this.state.options.get(OptionsEnum.showErrorsOnSubmit, this),
       })
         .then(({ isValid }: any) => {
           const handler = exec(isValid);
           if (isValid) return handler;
-          const $err = this.state.options.get("defaultGenericError", this);
-          const $throw = this.state.options.get("submitThrowsError", this);
+          const $err = this.state.options.get(OptionsEnum.defaultGenericError, this);
+          const $throw = this.state.options.get(OptionsEnum.submitThrowsError, this);
           if ($throw && $err) (this as any).invalidate();
           return handler;
         })
@@ -379,7 +381,7 @@ export default class Base implements BaseInterface {
 
   deepUpdate(fields: any, path: string = "", recursion: boolean = true, raw?: any): void {
     _.each(fields, (field, key) => {
-      const $key = _.has(field, "name") ? field.name : key;
+      const $key = _.has(field, FieldPropsEnum.name) ? field.name : key;
       const $path = _.trimStart(`${path}.${$key}`, ".");
       const $field = this.select($path, null, false);
       const $container =
@@ -394,7 +396,7 @@ export default class Base implements BaseInterface {
           });
         }
         else if (field?.fields) {
-          const fallback = this.state.options.get("fallback");
+          const fallback = this.state.options.get(OptionsEnum.fallback);
           if (!fallback && $field.fields.size === 0) {
             $field.value = parseInput($field.$input, {
               separated: _.get(raw, $path),
@@ -469,12 +471,12 @@ export default class Base implements BaseInterface {
 
         if (_.isString(prop)) {
           const removeValue =
-            prop === "value" &&
-            ((this.state.options.get("retrieveOnlyDirtyValues", this) &&
+            prop === FieldPropsEnum.value &&
+            ((this.state.options.get(OptionsEnum.retrieveOnlyDirtyValues, this) &&
               field.isPristine) ||
-              (this.state.options.get("retrieveOnlyEnabledFields", this) &&
+              (this.state.options.get(OptionsEnum.retrieveOnlyEnabledFields, this) &&
                 field.disabled) ||
-              (this.state.options.get("softDelete", this) && field.deleted));
+              (this.state.options.get(OptionsEnum.softDelete, this) && field.deleted));
 
           if (field.fields.size === 0) {
             delete obj[field.key]; // eslint-disable-line
@@ -485,7 +487,7 @@ export default class Base implements BaseInterface {
           }
 
           let value = this.deepGet(prop, field.fields);
-          if (prop === "value") value = field.$output(value);
+          if (prop === FieldPropsEnum.value) value = field.$output(value);
 
           delete obj[field.key]; // eslint-disable-line
           if (removeValue) return obj;
@@ -514,7 +516,7 @@ export default class Base implements BaseInterface {
     // UPDATE CUSTOM PROP
     if (_.isString(prop) && !_.isUndefined(data)) {
       allowedProps("field", [prop]);
-      const deep = (_.isObject(data) && prop === "value") || _.isPlainObject(data);
+      const deep = (_.isObject(data) && prop === FieldPropsEnum.value) || _.isPlainObject(data);
       if (deep && this.hasNestedFields) this.deepSet(prop, data, "", true);
       else _.set(this, `$${prop}`, data);
 
@@ -527,8 +529,8 @@ export default class Base implements BaseInterface {
 
     // NO PROP NAME PROVIDED ("prop" is value)
     if (_.isNil(data)) {
-      if (this.hasNestedFields) this.deepSet("value", prop, "", true);
-      else this.set("value", prop);
+      if (this.hasNestedFields) this.deepSet(FieldPropsEnum.value, prop, "", true);
+      else this.set(FieldPropsEnum.value, prop);
     }
   }
 
@@ -542,7 +544,7 @@ export default class Base implements BaseInterface {
     recursion: boolean = false
   ): void {
     const err = "You are updating a not existent field:";
-    const isStrict = this.state.options.get("strictUpdate", this);
+    const isStrict = this.state.options.get(OptionsEnum.strictUpdate, this);
 
     if (_.isNil(data)) {
       this.each((field: any) => field.clear(true));
@@ -582,7 +584,7 @@ export default class Base implements BaseInterface {
 
       this.$changed ++;
       this.state.form.$changed ++;
-      return this.execHook('onChange');
+      return this.execHook(FieldPropsEnum.onChange);
     }
 
     let key;
@@ -604,7 +606,7 @@ export default class Base implements BaseInterface {
     Del Field
    */
   del($path: string | null = null) {
-    const isStrict = this.state.options.get("strictDelete", this);
+    const isStrict = this.state.options.get(OptionsEnum.strictDelete, this);
     const path = parsePath($path ?? this.path);
     const fullpath = _.trim([this.path, path].join("."), ".");
     const container = this.container($path);
@@ -619,7 +621,7 @@ export default class Base implements BaseInterface {
     this.$changed ++;
     this.state.form.$changed ++;
 
-    if (this.state.options.get("softDelete", this)) {
+    if (this.state.options.get(OptionsEnum.softDelete, this)) {
       return this.select(fullpath).set("deleted", true);
     }
 
@@ -633,7 +635,7 @@ export default class Base implements BaseInterface {
   /**
     MobX Event (observe/intercept)
    */
-  MOBXEvent({ path = null, key = "value", call, type }: any): void {
+  MOBXEvent({ path = null, key = FieldPropsEnum.value, call, type }: any): void {
     const $instance = this.select(path || this.path, null, null) || this;
 
     const $call = (change: any) =>
@@ -693,7 +695,7 @@ export default class Base implements BaseInterface {
   /**
     Dispose Single Event (observe/intercept)
    */
-  disposeSingle({ type, key = "value", path = null }: any) {
+  disposeSingle({ type, key = FieldPropsEnum.value, path = null }: any) {
     const $path = parsePath(path ?? this.path);
     // eslint-disable-next-line
     if (type === "interceptor") key = `$${key}`; // target observables
@@ -832,48 +834,48 @@ export default class Base implements BaseInterface {
     Fields Values (recursive with Nested Fields)
    */
   values() {
-    return this.get("value");
+    return this.get(FieldPropsEnum.value);
   }
 
   /**
     Fields Errors (recursive with Nested Fields)
    */
   errors() {
-    return this.get("error");
+    return this.get(FieldPropsEnum.error);
   }
 
   /**
     Fields Labels (recursive with Nested Fields)
    */
   labels() {
-    return this.get("label");
+    return this.get(FieldPropsEnum.label);
   }
 
   /**
     Fields Placeholders (recursive with Nested Fields)
    */
   placeholders() {
-    return this.get("placeholder");
+    return this.get(FieldPropsEnum.placeholder);
   }
 
   /**
     Fields Default Values (recursive with Nested Fields)
    */
   defaults() {
-    return this.get("default");
+    return this.get(FieldPropsEnum.placeholder);
   }
 
   /**
     Fields Initial Values (recursive with Nested Fields)
    */
   initials() {
-    return this.get("initial");
+    return this.get(FieldPropsEnum.initial);
   }
 
   /**
     Fields Types (recursive with Nested Fields)
    */
   types() {
-    return this.get("type");
+    return this.get(FieldPropsEnum.type);
   }
 }

src/Field.ts
@@ -21,19 +21,20 @@ import {
   defaultClearValue,
 } from "./parser";
 
-import OptionsModel from "./models/OptionsModel";
+import OptionsModel, { OptionsEnum } from "./models/OptionsModel";
 import FieldInterface, { FieldConstructor } from "./models/FieldInterface";
+import { FieldPropsEnum } from "./models/FieldProps";
 
-const setupFieldProps = (instance: Field, props: any, data: any) =>
+const setupFieldProps = (instance: FieldInterface, props: any, data: any) =>
   Object.assign(instance, {
     $label: props.$label || (data && data.label) || "",
     $placeholder: props.$placeholder || (data && data.placeholder) || "",
     $disabled: props.$disabled || (data && data.disabled) || false,
-    $bindings: props.$bindings || (data && data.bindings) || "default",
+    $bindings: props.$bindings || (data && data.bindings) || FieldPropsEnum.default,
     $related: props.$related || (data && data.related) || [],
     $validators: toJS(props.$validators || (data && data.validators) || null),
     $validatedWith:
-      props.$validatedWith || (data && data.validatedWith) || "value",
+      props.$validatedWith || (data && data.validatedWith) || FieldPropsEnum.value,
     $rules: props.$rules || (data && data.rules) || null,
     $observers: props.$observers || (data && data.observers) || null,
     $interceptors: props.$interceptors || (data && data.interceptors) || null,
@@ -209,8 +210,8 @@ export default class Field extends Base implements FieldInterface {
 
     this.debouncedValidation = _.debounce(
       this.validate,
-      this.state.options.get("validationDebounceWait", this),
-      this.state.options.get("validationDebounceOptions", this)
+      this.state.options.get(OptionsEnum.validationDebounceWait, this),
+      this.state.options.get(OptionsEnum.validationDebounceOptions, this)
     );
 
     this.observeValidationOnBlur();
@@ -219,11 +220,11 @@ export default class Field extends Base implements FieldInterface {
     this.initMOBXEvent("observers");
     this.initMOBXEvent("interceptors");
 
-    this.execHook("onInit");
+    this.execHook(FieldPropsEnum.onInit);
 
     // handle Field onChange Hook for nested fields
-    this.hasNestedFields 
-      && autorun(() => this.changed && this.execHook('onChange'));
+    this.hasNestedFields
+      && autorun(() => this.changed && this.execHook(FieldPropsEnum.onChange));
   }
 
   /* ------------------------------------------------------------------ */
@@ -244,13 +245,13 @@ export default class Field extends Base implements FieldInterface {
   }
 
   get value() {
-    return this.getComputedProp("value");
+    return this.getComputedProp(FieldPropsEnum.value);
   }
 
   set value(newVal) {
     if (this.$value === newVal) return;
     // handle numbers
-    if (this.state.options.get("autoParseNumbers", this) === true) {
+    if (this.state.options.get(OptionsEnum.autoParseNumbers, this) === true) {
       if (_.isNumber(this.$initial)) {
         if (
           new RegExp("^-?\\d+(,\\d+)*(\\.\\d+([eE]\\d+)?)?$", "g").exec(newVal)
@@ -275,13 +276,13 @@ export default class Field extends Base implements FieldInterface {
   get initial() {
     return this.$initial
       ? toJS(this.$initial)
-      : this.getComputedProp("initial");
+      : this.getComputedProp(FieldPropsEnum.initial);
   }
 
   get default() {
     return this.$default
       ? toJS(this.$default)
-      : this.getComputedProp("default");
+      : this.getComputedProp(FieldPropsEnum.default);
   }
 
   set initial(val) {
@@ -346,11 +347,11 @@ export default class Field extends Base implements FieldInterface {
   }
 
   get hasError(): boolean {
-    return this.checkValidationErrors || this.check("hasError", true);
+    return this.checkValidationErrors || this.check(FieldPropsEnum.hasError, true);
   }
 
   get isValid(): boolean {
-    return !this.checkValidationErrors && this.check("isValid", true);
+    return !this.checkValidationErrors && this.check(FieldPropsEnum.isValid, true);
   }
 
   get isDefault(): boolean {
@@ -366,7 +367,7 @@ export default class Field extends Base implements FieldInterface {
   }
 
   get isEmpty(): boolean {
-    if (this.hasNestedFields) return this.check("isEmpty", true);
+    if (this.hasNestedFields) return this.check(FieldPropsEnum.isEmpty, true);
     if (_.isBoolean(this.value)) return !!this.$value;
     if (_.isNumber(this.value)) return false;
     if (_.isDate(this.value)) return false;
@@ -380,23 +381,23 @@ export default class Field extends Base implements FieldInterface {
   }
 
   get clearing(): boolean {
-    return this.hasNestedFields ? this.check("clearing", true) : this.$clearing;
+    return this.hasNestedFields ? this.check(FieldPropsEnum.clearing, true) : this.$clearing;
   }
 
   get focused(): boolean {
-    return this.hasNestedFields ? this.check("focused", true) : this.$focused;
+    return this.hasNestedFields ? this.check(FieldPropsEnum.focused, true) : this.$focused;
   }
 
   get blurred(): boolean {
-    return this.hasNestedFields ? this.check("blurred", true) : this.$blurred;
+    return this.hasNestedFields ? this.check(FieldPropsEnum.blurred, true) : this.$blurred;
   }
 
   get touched(): boolean {
-    return this.hasNestedFields ? this.check("touched", true) : this.$touched;
+    return this.hasNestedFields ? this.check(FieldPropsEnum.touched, true) : this.$touched;
   }
 
   get deleted(): boolean {
-    return this.hasNestedFields ? this.check("deleted", true) : this.$deleted;
+    return this.hasNestedFields ? this.check(FieldPropsEnum.deleted, true) : this.$deleted;
   }
 
   /* ------------------------------------------------------------------ */
@@ -427,13 +428,13 @@ export default class Field extends Base implements FieldInterface {
   onChange = (...args: any) =>
     this.type === "file"
       ? this.onDrop(...args)
-      : this.execHandler("onChange", args, this.sync);
+      : this.execHandler(FieldPropsEnum.onChange, args, this.sync);
 
-  onToggle = (...args: any) => this.execHandler("onToggle", args, this.sync);
+  onToggle = (...args: any) => this.execHandler(FieldPropsEnum.onToggle, args, this.sync);
 
   onBlur = (...args: any) =>
     this.execHandler(
-      "onBlur",
+      FieldPropsEnum.onBlur,
       args,
       action(() => {
         if (!this.$blurred) {
@@ -446,7 +447,7 @@ export default class Field extends Base implements FieldInterface {
 
   onFocus = (...args: any) =>
     this.execHandler(
-      "onFocus",
+      FieldPropsEnum.onFocus,
       args,
       action(() => {
         this.$focused = true;
@@ -456,7 +457,7 @@ export default class Field extends Base implements FieldInterface {
 
   onDrop = (...args: any) =>
     this.execHandler(
-      "onDrop",
+      FieldPropsEnum.onDrop,
       args,
       action(() => {
         const e = args[0];
@@ -479,7 +480,7 @@ export default class Field extends Base implements FieldInterface {
   ): void {
     this.key = $key;
     this.path = $path;
-    this.id = this.state.options.get("uniqueId")?.apply(this, [this]);
+    this.id = this.state.options.get(OptionsEnum.uniqueId)?.apply(this, [this]);
     const struct = this.state.struct();
     const structPath = pathToStruct(this.path);
     const isEmptyArray: boolean = Array.isArray(struct)
@@ -558,7 +559,7 @@ export default class Field extends Base implements FieldInterface {
   getComputedProp(key: string): any {
     if (this.incremental || this.hasNestedFields) {
       const $val =
-        key === "value"
+        key === FieldPropsEnum.value
           ? this.get(key, false)
           : untracked(() => this.get(key, false));
 
@@ -655,7 +656,7 @@ export default class Field extends Base implements FieldInterface {
     if (deep) this.each((field: any) => field.clear(true));
 
     this.validate({
-      showErrors: this.state.options.get("showErrorsOnClear", this),
+      showErrors: this.state.options.get(OptionsEnum.showErrorsOnClear, this),
     });
   }
 
@@ -665,7 +666,7 @@ export default class Field extends Base implements FieldInterface {
     this.$touched = false;
     this.$blurred = false;
     this.$changed = 0;
-    
+
     const useDefaultValue = this.$default !== this.$initial;
     if (useDefaultValue) this.value = this.$default;
     if (!useDefaultValue) this.value = this.$initial;
@@ -674,7 +675,7 @@ export default class Field extends Base implements FieldInterface {
     if (deep) this.each((field: FieldInterface) => field.reset(true));
 
     this.validate({
-      showErrors: this.state.options.get("showErrorsOnReset", this),
+      showErrors: this.state.options.get(OptionsEnum.showErrorsOnReset, this),
     });
   }
 
@@ -700,14 +701,14 @@ export default class Field extends Base implements FieldInterface {
 
   observeValidationOnBlur(): void {
     const opt = this.state.options;
-    if (opt.get("validateOnBlur", this)) {
+    if (opt.get(OptionsEnum.validateOnBlur, this)) {
       this.disposeValidationOnBlur = observe(
         this,
         "$focused",
         (change) =>
           change.newValue === false &&
           this.debouncedValidation({
-            showErrors: opt.get("showErrorsOnBlur", this),
+            showErrors: opt.get(OptionsEnum.showErrorsOnBlur, this),
           })
       );
     }
@@ -715,31 +716,31 @@ export default class Field extends Base implements FieldInterface {
 
   observeValidationOnChange(): void {
     const opt = this.state.options;
-    if (opt.get("validateOnChange", this)) {
+    if (opt.get(OptionsEnum.validateOnChange, this)) {
       this.disposeValidationOnChange = observe(
         this,
         "$value",
         () =>
           !this.actionRunning &&
           this.debouncedValidation({
-            showErrors: opt.get("showErrorsOnChange", this),
+            showErrors: opt.get(OptionsEnum.showErrorsOnChange, this),
           })
       );
     } else if (
-      opt.get("validateOnChangeAfterInitialBlur", this) ||
-      opt.get("validateOnChangeAfterSubmit", this)
+      opt.get(OptionsEnum.validateOnChangeAfterInitialBlur, this) ||
+      opt.get(OptionsEnum.validateOnChangeAfterSubmit, this)
     ) {
       this.disposeValidationOnChange = observe(
         this,
         "$value",
         () =>
           !this.actionRunning &&
-          ((opt.get("validateOnChangeAfterInitialBlur", this) &&
+          ((opt.get(OptionsEnum.validateOnChangeAfterInitialBlur, this) &&
             this.blurred) ||
-            (opt.get("validateOnChangeAfterSubmit", this) &&
+            (opt.get(OptionsEnum.validateOnChangeAfterSubmit, this) &&
               this.state.form.submitted)) &&
           this.debouncedValidation({
-            showErrors: opt.get("showErrorsOnChange", this),
+            showErrors: opt.get(OptionsEnum.showErrorsOnChange, this),
           })
       );
     }
@@ -753,7 +754,7 @@ export default class Field extends Base implements FieldInterface {
     if (type === "observers") fn = this.observe;
     if (type === "interceptors") fn = this.intercept;
     // @ts-ignore
-    this[`$${type}`].map((obj: any) => fn(_.omit(obj, "path")));
+    this[`$${type}`].map((obj: any) => fn(_.omit(obj, FieldPropsEnum.path)));
   }
 
   bind(props = {}) {
@@ -764,7 +765,7 @@ export default class Field extends Base implements FieldInterface {
     if (!_.isPlainObject(fields)) {
       throw new Error("The update() method accepts only plain objects.");
     }
-    const fallback = this.state.options.get("fallback");
+    const fallback = this.state.options.get(OptionsEnum.fallback);
     if (!fallback && this.fields.size === 0) {
       this.value = parseInput(this.$input, {
         separated: fields,

src/Form.ts
@@ -11,6 +11,8 @@ import FormInterface, {
   FieldsDefinitions,
   FormConfig,
 } from "./models/FormInterface";
+import { FieldPropsEnum } from "./models/FieldProps";
+import { OptionsEnum } from "./models/OptionsModel";
 
 export default class Form extends Base implements FormInterface {
   name: string;
@@ -86,21 +88,20 @@ export default class Form extends Base implements FormInterface {
 
     this.debouncedValidation = _.debounce(
       this.validate,
-      this.state.options.get("validationDebounceWait"),
-      this.state.options.get("validationDebounceOptions")
+      this.state.options.get(OptionsEnum.validationDebounceWait),
+      this.state.options.get(OptionsEnum.validationDebounceOptions)
     );
 
     // execute validation on form initialization
-    if (this.state.options.get("validateOnInit") === true) {
-      this.validator.validate({
-        showErrors: this.state.options.get("showErrorsOnInit"),
+    this.state.options.get(OptionsEnum.validateOnInit)
+      && this.validator.validate({
+        showErrors: this.state.options.get(OptionsEnum.showErrorsOnInit),
       });
-    }
 
-    this.execHook("onInit");
+    this.execHook(FieldPropsEnum.onInit);
 
     // handle Form onChange Hook
-    autorun(() => this.$changed && this.execHook('onChange'));
+    autorun(() => this.$changed && this.execHook(FieldPropsEnum.onChange));
   }
 
   /* ------------------------------------------------------------------ */
@@ -114,11 +115,11 @@ export default class Form extends Base implements FormInterface {
   }
 
   get clearing(): boolean {
-    return this.check("clearing", true);
+    return this.check(FieldPropsEnum.clearing, true);
   }
 
   get resetting(): boolean {
-    return this.check("resetting", true);
+    return this.check(FieldPropsEnum.resetting, true);
   }
 
   get error(): string | null {
@@ -126,39 +127,39 @@ export default class Form extends Base implements FormInterface {
   }
 
   get hasError(): boolean {
-    return !!this.validator.error || this.check("hasError", true);
+    return !!this.validator.error || this.check(FieldPropsEnum.hasError, true);
   }
 
   get isValid(): boolean {
-    return !this.validator.error && this.check("isValid", true);
+    return !this.validator.error && this.check(FieldPropsEnum.isValid, true);
   }
 
   get isPristine(): boolean {
-    return this.check("isPristine", true);
+    return this.check(FieldPropsEnum.isPristine, true);
   }
 
   get isDirty(): boolean {
-    return this.check("isDirty", true);
+    return this.check(FieldPropsEnum.isDirty, true);
   }
 
   get isDefault(): boolean {
-    return this.check("isDefault", true);
+    return this.check(FieldPropsEnum.isDefault, true);
   }
 
   get isEmpty(): boolean {
-    return this.check("isEmpty", true);
+    return this.check(FieldPropsEnum.isEmpty, true);
   }
 
   get focused(): boolean {
-    return this.check("focused", true);
+    return this.check(FieldPropsEnum.focused, true);
   }
 
   get touched(): boolean {
-    return this.check("touched", true);
+    return this.check(FieldPropsEnum.touched, true);
   }
 
   get disabled(): boolean {
-    return this.check("disabled", true);
+    return this.check(FieldPropsEnum.disabled, true);
   }
 
   makeField(data: FieldConstructor) {
@@ -181,7 +182,7 @@ export default class Form extends Base implements FormInterface {
 
   invalidate(message: string | null = null): void {
     this.validator.error =
-      message || this.state.options.get("defaultGenericError") || true;
+      message || this.state.options.get(OptionsEnum.defaultGenericError) || true;
   }
 
   showErrors(show: boolean = true): void {

commit 280e8cffbb098049a288340f7d44486380338c9e

fix(base.ts): fix form defaults()
It seems to me, that there was a bug introduced with commit
https://github.com/foxhound87/mobx-react-form/commit/26123aa7718871c6e9ddec67e1699e949d096ece#diff-65addcde6fff5229abdf5708736cabd07f335e865002080cd102d2fbb3ec67a8R865.

src/Base.ts
@@ -941,7 +941,7 @@ export default class Base implements BaseInterface {
     Fields Default Values (recursive with Nested Fields)
    */
   defaults() {
-    return this.get(FieldPropsEnum.placeholder);
+    return this.get(FieldPropsEnum.default);
   }
 
   /**

tests/data/_.flat.ts
@@ -17,6 +17,7 @@ import $O from "./forms/flat/form.o";
 import $P from "./forms/flat/form.p";
 import $Q from "./forms/flat/form.q";
 import $R from "./forms/flat/form.r";
+import $T from "./forms/flat/form.t";
 
 export default {
   $A,
@@ -35,4 +36,5 @@ export default {
   $P,
   $Q,
   $R,
+  $T,
 };

tests/data/forms/flat/form.t.ts
@@ -1,0 +1,34 @@
+import { Form } from "../../../../src";
+
+const fields = ["username", "email", "country"];
+
+const values = {
+  username: "",
+  email: "",
+  country: "",
+};
+
+const defaults = {
+  username: "TestUser",
+  email: "s.jobs@apple.com",
+  country: "USA",
+};
+
+const labels = {
+  username: "User name",
+  email: "Email",
+  country: "Country",
+};
+
+class NewForm extends Form {
+}
+
+export default new NewForm(
+  {
+    fields,
+    values,
+    defaults,
+    labels,
+  },
+  { name: "Flat-T" }
+);

tests/flat.actions.ts
@@ -167,3 +167,15 @@ describe('Field values type checks', () => {
     assert.isNumber($.$A.$('assets').value,
       '$A assets.value is not a number'));
 });
+
+describe('Form default values', () => {
+  it('$T defaults is an object', () =>
+    assert.isObject($.$T.defaults(),
+      '$T defaults is not an object'));
+
+  it('$T defaults check default field values', () => {
+    expect($.$T.defaults().username).to.be.equal('TestUser');
+    expect($.$T.defaults().email).to.be.equal('s.jobs@apple.com');
+    expect($.$T.defaults().country).to.be.equal('USA');
+  });
+});